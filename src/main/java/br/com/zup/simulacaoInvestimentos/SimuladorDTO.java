675115dedc28ca1241294bc51f686eb61ed1f96e
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-04 14:35:42 -0300

    Criar Classe Simulador DTO

---
+++ src/main/java/br/com/zup/simulacaoInvestimentos/SimuladorDTO.java
@@ -0,0 +1,40 @@
+package br.com.zup.simulacaoInvestimentos;
+
+public class SimuladorDTO {
+    private double valorInvestimento;
+    private double lucro;
+    private double valorFinal;
+
+    public SimuladorDTO(double valorInvestimento, double lucro, double valorFinal) {
+        this.valorInvestimento = valorInvestimento;
+        this.lucro = lucro;
+        this.valorFinal = valorFinal;
+    }
+
+    public SimuladorDTO() {
+    }
+
+    public double getValorInvestimento() {
+        return valorInvestimento;
+    }
+
+    public void setValorInvestimento(double valorInvestimento) {
+        this.valorInvestimento = valorInvestimento;
+    }
+
+    public double getLucro() {
+        return lucro;
+    }
+
+    public void setLucro(double lucro) {
+        this.lucro = lucro;
+    }
+
+    public double getValorFinal() {
+        return valorFinal;
+    }
+
+    public void setValorFinal(double valorFinal) {
+        this.valorFinal = valorFinal;
+    }
+}
